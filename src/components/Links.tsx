c21460a59dc3c7a31a4df2b9f6dc9102f937ebaf
Üsame <usame.kilicbay@gmail.com>
2024-07-20 10:19:19 +0700

    add: BMAC icon

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -28,6 +28,24 @@ export default function Links() {
           <LiaGithub className="size-6"></LiaGithub>
         </Link>
       </Flex>
+      {/* <Link
+        href="https://www.buymeacoffee.com/usame.kilicbay"
+        isExternal
+        _hover={{
+          textColor: "purple.500",
+          transform: "scale(1.3)",
+          transition: "transform 0.3s ease-in-out",
+        }}
+      >
+        <Box>
+          <Image
+            src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+            alt="Buy Me A Coffee"
+            h="8"
+            w="30"
+          ></Image>
+        </Box>
+      </Link> */}
     </>
   );
 }
